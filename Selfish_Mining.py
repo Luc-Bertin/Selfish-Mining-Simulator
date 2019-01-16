85cf758958bc0119a6cd4804daba666a24fc849c
Luc Bertin <lucbertin@outlook.com>
2019-01-16 10:44:32 +0100

    Resolved issue affecting honest miners revenue on gamma, updated final simulation results

--- Selfish_Mining.py
+++ Selfish_Mining.py
@@ -7,106 +7,113 @@ class Selfish_Mining:
 	def __init__(self, **d):
 		self.__nb_simulations = d['nb_simulations']
 		self.__delta = 0 # advance of selfish miners on honests'ones
-		self.__private_chain_length = 0 # length of private chain RESET at each validation
-		self.__public_chain_length = 0 # length of public chain RESET at each validation
-		self.__blocked_validated_for_honests = 0
-		self.__blocked_validated_for_selfish = 0
-		self.__revenue = None
+		self.__privateChain = 0 # length of private chain RESET at each validation
+		self.__publicChain = 0 # length of public chain RESET at each validation
+		self.__honestsValidBlocks = 0
+		self.__selfishValidBlocks = 0
 		self.__counter = 1
 
 		# Setted Parameters
 		self.__alpha = d['alpha']
 		self.__gamma = d['gamma']
 		
-		self.__end_result = None
+		# For results
+		self.__revenue = None
+		self.__orphanBlocks = 0
+		self.__totalMinedBlocks = 0
 
 	def write_file(self):
+		stats_result = [self.__alpha, self.__gamma, self.__nb_simulations,\
+						self.__honestsValidBlocks, self.__selfishValidBlocks,\
+						self.__revenue, self.__orphanBlocks, self.__totalMinedBlocks]
 		with open('results.txt', 'a', encoding='utf-8') as f:
-			f.write(','.join(self.__end_result) + '\n')
+			f.write(','.join([str(x) for x in stats_result]) + '\n')
 
 	def Simulate(self):
 		while(self.__counter <= self.__nb_simulations):
 			# Mining power does not mean the block is actually found
 			# there is a probability p to find it
 			r = random.uniform(0, 1) # random number for each simulation
-			self.__delta = self.__private_chain_length - self.__public_chain_length
+			self.__delta = self.__privateChain - self.__publicChain
 			if r <= float(self.__alpha):
 				self.On_Selfish_Miners()
 			else:
 				self.On_Honest_Miners()
 			### COPY-PASTE THE 3 LINES BELOW IN THE IF/ELSE TO GET EACH ITERATION RESULTS ###
-			#self.__revenue = self.compute_revenue()
+			#self.actualize_results()
 			#print(self)
 			#time.sleep(1)
 			self.__counter += 1
 
-		#must take into account when private chain is not empty and simulation ends
-		self.__delta = self.__private_chain_length - self.__public_chain_length
+		# Publishing private chain if not empty when total nb of simulations reached
+		self.__delta = self.__privateChain - self.__publicChain
 		if self.__delta > 0:
-			self.__blocked_validated_for_selfish += self.__private_chain_length
-			self.__public_chain_length, self.__private_chain_length = 0,0
-		self.__revenue = self.compute_revenue()
-		
-		result = [self.__alpha, self.__gamma, self.__nb_simulations, self.__blocked_validated_for_honests,\
-		 self.__blocked_validated_for_selfish, self.__revenue]
-		self.__end_result = [str(x) for x in result]
+			self.__selfishValidBlocks += self.__privateChain
+			self.__publicChain, self.__privateChain = 0,0
+		self.actualize_results()
 		print(self)
 
 	def On_Selfish_Miners(self):
-		self.__private_chain_length += 1
-		if self.__delta == 0 and self.__private_chain_length == 2:
-			self.__private_chain_length, self.__public_chain_length = 0,0
-			self.__blocked_validated_for_selfish += 2
-			#don't forget to retrieve money 
-			#publishing the private chain reset both public and private chain length
+		self.__privateChain += 1
+		if self.__delta == 0 and self.__privateChain == 2:
+			self.__privateChain, self.__publicChain = 0,0
+			self.__selfishValidBlocks += 2
+			# Publishing private chain reset both public and private chains lengths to 0
 
 	def On_Honest_Miners(self): 
-		self.__public_chain_length += 1
+		self.__publicChain += 1
 		if self.__delta == 0:
-			self.__blocked_validated_for_honests += 1
-			#in all cases either, bloc mined by honest will be validated anyway
-			# 1) if it appends private chain from delta=0 => validated in the revenue
-			# 2) if it appends public chain from delta=0 => validated de facto
-			#if self.__private_chain_length == 0: 
-			#	self.__blocked_validated_for_honests += 1
-			# gamma will matter in this case only (competitive case)
-			# Reminder: gamma = ratio of honest miners who choose to mine on pool's block
-			if self.__private_chain_length > 0 and random.uniform(0, 1) <= float(self.__gamma):
-				self.__blocked_validated_for_selfish += 1
-				#g = random.uniform(0, 1)
-				#if g <= float(self.__gamma):
-				
+			# if 1 block is found => 1 block validated as honest miners take advance
+			self.__honestsValidBlocks += 1
+			# If there is a competition though (1-1) considering gamma,
+			# (Reminder: gamma = ratio of honest miners who choose to mine on pool's block)
+			# --> either it appends the private chain => 1 block for each competitor in revenue
+			# --> either it appends the honnest chain => 2 blocks for honnest miners (1 more then)
+			s = random.uniform(0, 1)
+			if self.__privateChain > 0 and s <= float(self.__gamma):
+				self.__selfishValidBlocks += 1
+			elif self.__privateChain > 0 and s > float(self.__gamma):
+				self.__honestsValidBlocks += 1
 			#in all cases (append private or public chain) all is reset to 0
-			self.__private_chain_length, self.__public_chain_length = 0,0
+			self.__privateChain, self.__publicChain = 0,0
 		
 		elif self.__delta == 2:
-			self.__blocked_validated_for_selfish += self.__private_chain_length
-			self.__public_chain_length, self.__private_chain_length = 0,0
+			self.__selfishValidBlocks += self.__privateChain
+			self.__publicChain, self.__privateChain = 0,0
 
-	def compute_revenue(self):
-		if self.__blocked_validated_for_honests or self.__blocked_validated_for_selfish:
-			return(round(self.__blocked_validated_for_selfish/(self.__blocked_validated_for_selfish +
-			 self.__blocked_validated_for_honests),2))
+	def actualize_results(self):
+		# Total Blocks Mined
+		self.__totalMinedBlocks = self.__honestsValidBlocks + self.__selfishValidBlocks
+		# Orphan Blocks 
+		self.__orphanBlocks = self.__nb_simulations - self.__totalMinedBlocks
+		# Revenue
+		if self.__honestsValidBlocks or self.__selfishValidBlocks:
+			self.__revenue = 100*round(self.__selfishValidBlocks/(self.__totalMinedBlocks),3)
 
-	#show message
+	# Show message
 	def __str__(self):
 		if self.__counter <= self.__nb_simulations:
 			simulation_message = '\nSimulation ' + str(self.__counter) + ' out of ' + str(self.__nb_simulations) + '\n'
+			current_stats = 'Private chain : ' + '+ '*int(self.__privateChain) + '\n'\
+			'public chain : ' + '+ '*int(self.__publicChain) + '\n'
 		else:
-			simulation_message = '\n' + str(self.__nb_simulations) + ' Simulations Done // publishing private chain if non-empty\n'
-		choosen_parameters = 'alpha : ' + str(self.__alpha) + '\t||\t' +'gamma : ' + str(self.__gamma) +'\n'
-		private_public_results = \
-			'private chain : ' + '+ '*int(self.__private_chain_length) + '\n'\
-			'public chain : ' + '+ '*int(self.__public_chain_length) + '\n'\
-			'blocks validated by honests miners : ' + str(self.__blocked_validated_for_honests) + '\n'\
-			'blocks validated by selfish miners : ' + str(self.__blocked_validated_for_selfish) + '\n'\
-			'delta was before this : ' + str(self.__delta) + '\n'\
-			'current revenue ratio \n\t Rpool = rpool / (rpool + rothers) : ' + str(self.__revenue) + '\n'		
-		return simulation_message + choosen_parameters + private_public_results
+			simulation_message = '\n\n' + str(self.__nb_simulations) + ' Simulations Done // publishing private chain if non-empty\n'
+			current_stats = ''
 
-dico = {'nb_simulations':200000, 'alpha':0.5, 'gamma':0.4}
+		choosen_parameters = 'Alpha : ' + str(self.__alpha) + '\t||\t' +'Gamma : ' + str(self.__gamma) +'\n'
+
+		selfish_vs_honests_stats = \
+			'Blocks validated by honest miners : ' + str(self.__honestsValidBlocks) + '\n'\
+			'Blocks validated by selfish miners : ' + str(self.__selfishValidBlocks) + '\n'\
+			'Number of total blocks mined : ' + str(self.__totalMinedBlocks) + '\n'\
+			'Number of Orphan blocks : ' + str(self.__orphanBlocks) + '\n'\
+			'Revenue ratio = PoolBlocks / TotalBlocks : ' + str(self.__revenue) + '%\n'
+		return simulation_message + current_stats + choosen_parameters + selfish_vs_honests_stats
+
+dico = {'nb_simulations':250000, 'alpha':0.35, 'gamma':0.5}
 new = Selfish_Mining(**dico)
 new.Simulate()
+
 
 """
 ### TO SAVE MULTIPLE VALUES IN FILE ###
